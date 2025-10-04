7642a102ba40d0510c3187763739022023819630
maira-franco <maira.franco@istea.com.ar>
2025-10-03 21:34:27 -0300

    Update MainActivity.kt

--- app/src/main/java/com/example/parcial_billetera/MainActivity.kt
+++ app/src/main/java/com/example/parcial_billetera/MainActivity.kt
@@ -1,17 +1,17 @@
-package com.example.parcial
+package com.example.parcial_billetera
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.parcial.ui.theme.ParcialTheme
+import com.example.parcial_billetera.ui.theme.ParcialBilleteraTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ParcialTheme {
+            ParcialBilleteraTheme  {
                 BilleteraNav()
             }
         }
